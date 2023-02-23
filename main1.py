e8076b749a557eba358313cf3315cdcf5b68934f
Angelina <79199054+AngelinaStankevich@users.noreply.github.com>
2023-02-23 23:32:44 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,29 @@
+#1 задание
+a = 6
+b = 2
+print(a + b, a - b, a * b, a ** b, a % b, a // b)
+
+
+#2 задание
+x = ((17 / 2) * 3) + 2
+print(x)
+x = 2 + ((17 / 2) * 3)
+print(x)
+x = (19 % 4) + ((15 / 2) * 3)
+print(x)
+x = (15 + 6) - (10 * 4)
+print(x)
+x = ((17 / 2) % 2) * (3 ** 3)
+print(x)
+
+
+#3 задание
+print("Введите число x:")
+x = int(input())
+print("Введите число y:")
+y = int(input())
+result = (abs(x) - abs(y)) / (1 + abs(x * y))
+print(result)
+
+
+
